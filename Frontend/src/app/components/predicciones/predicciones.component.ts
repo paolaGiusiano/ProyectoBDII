76dbf885e1ac4da81e5ac4907fb8a53cee3d17c3
unknown <paola.giusiano@correo.ucu.edu.uy>
2024-06-01 23:41:13 -0300

    predicciiones

--- Frontend/src/app/components/predicciones/predicciones.component.ts
+++ Frontend/src/app/components/predicciones/predicciones.component.ts
@@ -1,100 +1,58 @@
-/*import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators, FormGroup, AbstractControl, ReactiveFormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
-import { PrediccionesService } from '../../services/predicciones.service';
-import { CommonModule } from '@angular/common';
-
-@Component({
-  selector: 'app-predicciones',
-  standalone: true,
-  imports: [ReactiveFormsModule, CommonModule],
-  templateUrl: './predicciones.component.html',
-  styleUrls: ['./predicciones.component.css']
-})
-export class PrediccionesComponent implements OnInit {
-  predictionForm: FormGroup = this.formBuilder.group({
-    prediccionLocal: ['', Validators.required],
-    prediccionVisitante: ['', Validators.required],
-    selectedMatch: ['', Validators.required],
-  });; 
-  matches: { id: number, name: string }[] = []; 
-
-  constructor(private formBuilder: FormBuilder, private router: Router, private prediccionService: PrediccionesService) {}
-
-  ngOnInit(): void {
-
-    // Aquí deberías cargar los partidos disponibles desde el servicio
-    this.matches = [
-      { id: 1, name: 'Partido 1' },
-      { id: 2, name: 'Partido 2' },
-      // Agrega más partidos según sea necesario
-    ];
-  }
-
-  prediccion(): void {
-    // Aquí deberías enviar la predicción al servicio para ser almacenada en la base de datos
-    console.log('Predicción enviada:', this.predictionForm.value);
-    // Limpia los campos después de enviar la predicción
-    this.predictionForm.reset();
-  }
-}*/
-
 
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators, FormGroup,  ReactiveFormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { PrediccionesService } from '../../services/predicciones.service';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-predicciones',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule, FormsModule],
   templateUrl: './predicciones.component.html',
   styleUrls: ['./predicciones.component.css']
 })
 export class PrediccionesComponent implements OnInit {
-  predictionForm!: FormGroup; // Añadir ! para indicar que será inicializado en ngOnInit
-  matches: { id: number, name: string }[] = []; 
+  upcomingMatches: any[] = [];
+  selectedMatch: any;
+  prediction: any = {
+    local: 0,
+    visitante: 0,
+    matchId: null
+  };
 
-  constructor(private formBuilder: FormBuilder, private router: Router, private prediccionService: PrediccionesService) {}
+  constructor(private predictionService: PrediccionesService) { }
 
   ngOnInit(): void {
-    this.predictionForm = this.formBuilder.group({
-      prediccionLocal: ['', Validators.required],
-      prediccionVisitante: ['', Validators.required],
-      selectedMatch: ['', Validators.required],
-    });
-
-    this.loadMatches();
+    this.loadUpcomingMatches();
   }
 
-  loadMatches(): void {
-    this.prediccionService.getMatches()
-      .subscribe(
-        (data: any) => {
-          this.matches = data;
-        },
-        (error) => {
-          console.error('Error loading matches:', error);
-        }
-      );
+  loadUpcomingMatches(): void {
+    this.predictionService.getUpcomingMatches().subscribe(matches => {
+      this.upcomingMatches = matches;
+      if (this.upcomingMatches.length > 0) {
+        this.selectedMatch = this.upcomingMatches[0];
+        this.prediction.matchId = this.selectedMatch.id;
+      }
+    });
+  }
+
+
+  transformMatchTime(): void {
+    this.upcomingMatches.forEach(match => {
+      match.hora = new Date('1970-01-01T' + match.hora); // Concatenamos la fecha con la hora
+    });
+  }
+
+
+  onMatchChange(event: any): void {
+    this.prediction.matchId = this.selectedMatch.id;
   }
 
   submitPrediction(): void {
-    if (this.predictionForm.valid) {
-      this.prediccionService.submitPrediction(this.predictionForm.value)
-        .subscribe(
-          (response) => {
-            console.log('Prediction submitted successfully:', response);
-            this.predictionForm.reset();
-          },
-          (error) => {
-            console.error('Error submitting prediction:', error);
-          }
-        );
-    } else {
-      console.error('Form validation failed. Please fill all required fields.');
-    }
+    this.predictionService.submitPrediction(this.prediction).subscribe(response => {
+      console.log('Predicción enviada con éxito', response);
+    }, error => {
+      console.error('Error al enviar la predicción', error);
+    });
   }
 }
