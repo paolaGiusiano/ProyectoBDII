d02675b1a2ae92cf929b632bf4b80620755aacd4
unknown <paola.giusiano@correo.ucu.edu.uy>
2024-06-04 16:17:03 -0300

    modificar predicciones

--- Frontend/src/app/components/ver-predicciones/ver-predicciones.component.ts
+++ Frontend/src/app/components/ver-predicciones/ver-predicciones.component.ts
@@ -1,22 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators, FormGroup, AbstractControl, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, Validators, ReactiveFormsModule, FormBuilder, FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { PrediccionesService } from '../../services/predicciones.service';
 import { AuthService } from '../../services/auth.service';
 import { CommonModule } from '@angular/common';
 
-
 @Component({
   selector: 'app-ver-predicciones',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule, FormsModule],
   templateUrl: './ver-predicciones.component.html',
   styleUrls: ['./ver-predicciones.component.css']
 })
 export class VerPrediccionesComponent implements OnInit {
   predictions: any[] = [];
+  showEditForm: boolean = false;
+  editingPrediction: any;
+   editingPredictionId: number | null = null;
+  editPredictionForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private router: Router, private predictionService: PrediccionesService, private authService: AuthService) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private predictionService: PrediccionesService,
+    private authService: AuthService
+  ) {
+    this.editPredictionForm = this.formBuilder.group({
+      id_partido: ['', Validators.required],
+      prediccion_local: ['', Validators.required],
+      prediccion_visitante: ['', Validators.required],
+      campeon: ['', Validators.required],
+      subcampeon: ['', Validators.required]
+    });
+  }
+
   ngOnInit(): void {
     this.loadPredictions();
   }
@@ -34,7 +51,6 @@ export class VerPrediccionesComponent implements OnInit {
     }
   }
 
-
   deletePrediction(id_prediccion: number): void {
     const documento = this.authService.getDocumento();
     if (documento) {
@@ -49,11 +65,61 @@ export class VerPrediccionesComponent implements OnInit {
   }
 
 
-
   editPrediction(prediction: any): void {
-    console.log('Editar predicción:', prediction);
+    console.log('Editando predicción', prediction);
+    this.showEditForm = true;
+    this.editingPredictionId = prediction.id;
+    const documento = this.authService.getDocumento();
+    this.editPredictionForm.patchValue({
+      documento_alumno: documento,
+      id_partido: prediction.id_partido,
+      prediccion_local: prediction.prediccion_local,
+      prediccion_visitante: prediction.prediccion_visitante,
+      campeon: prediction.campeon,
+      subcampeon: prediction.subcampeon
+    });
+
   }
 
 
+  updatePrediction(): void {
+    console.log('Form values:', this.editPredictionForm.value);
+    console.log('Editing Prediction ID:', this.editingPredictionId);
+    if (this.editingPredictionId !== null) {
+      const predictionData = {
+        id: this.editingPredictionId,
+        documento_alumno: this.authService.getDocumento(),
+        id_partido: this.editPredictionForm.value.id_partido,
+        prediccion_local: this.editPredictionForm.value.prediccion_local,
+        prediccion_visitante: this.editPredictionForm.value.prediccion_visitante,
+        campeon: this.editPredictionForm.value.campeon || '',
+        subcampeon: this.editPredictionForm.value.subcampeon || ''
+      };
+      console.log('Datos de la predicción:', predictionData);
+      this.predictionService.updatePrediction(predictionData).subscribe({
+        next: () => {
+          this.loadPredictions();
+          this.showEditForm = false;
+          this.editingPredictionId = null;
+        },
+        error: (error) => {
+          console.error('Error al actualizar la predicción', error);
+          if (error.status === 200 && typeof error.error.text === 'string') {
+            console.error('Parece que la respuesta no está en formato JSON:', error.error.text);
+          }
+        }
+      });
+    } else {
+      console.error('No se ha seleccionado ninguna predicción para actualizar');
+    }
+  }
+  
 
+
+  
+
+  cancelEdit(): void {
+    this.showEditForm = false;
+    this.editingPrediction = null;
+  }
 }
