5e832b588e506a7ee1036ffea93127c4a7c604d0
unknown <paola.giusiano@correo.ucu.edu.uy>
2024-06-16 14:21:16 -0300

    resultado eliminatorias

--- Frontend/src/app/components/ingreso-resultados/ingreso-resultados.component.ts
+++ Frontend/src/app/components/ingreso-resultados/ingreso-resultados.component.ts
@@ -14,6 +14,13 @@ interface Match {
   equipo_visitante: string;
 }
 
+interface Team {
+  nombre: string;
+  puntos: number;
+  golesFavor: number;
+  golesContra: number;
+}
+
 @Component({
   selector: 'app-ingreso-resultados',
   standalone: true,
@@ -23,6 +30,8 @@ interface Match {
 })
 export class IngresoResultadosComponent implements OnInit {
   matches: Match[] = [];
+  cuartosFinales: Match[] = [];
+  semifinales: Match[] = [];
   result: { [key: number]: { goles_local: number, goles_visitante: number } } = {};
 
   // Mapa de equipos a nombres de archivos de banderas
@@ -53,11 +62,19 @@ export class IngresoResultadosComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.partidosService.getPartidos() // Usa el servicio PartidosService
+    this.partidosService.getPartidos()
       .subscribe(data => {
-        this.matches = data;
-        // Initialize result object for each match
-        this.matches.forEach(match => {
+        // Fase de grupos 
+        this.matches = data.filter(match => match.id <= 24);
+  
+        // Cuartos de final 
+        this.cuartosFinales = data.filter(match => match.id >= 31 && match.id <= 34);
+  
+        // Semifinales 
+        this.semifinales = data.filter(match => match.id === 29 || match.id === 30);
+        
+
+        this.matches.concat(this.cuartosFinales, this.semifinales).forEach(match => {
           if (this.isLocalStorageAvailable()) {
             const savedResult = localStorage.getItem(`result_${match.id}`);
             if (savedResult) {
@@ -70,6 +87,9 @@ export class IngresoResultadosComponent implements OnInit {
           }
         });
       });
+      this.actualizarCuartos();
+      this.actualizarSemifinal();
+
   }
 
   saveResult(matchId: number): void {
@@ -81,18 +101,18 @@ export class IngresoResultadosComponent implements OnInit {
     }).subscribe(
       response => {
         console.log('Result saved', response);
-        // Save result to localStorage
+
         if (this.isLocalStorageAvailable()) {
           localStorage.setItem(`result_${matchId}`, JSON.stringify(result));
         }
-        // Show success snackBar
+
         this.snackBar.open('Resultado guardado con éxito', 'Cerrar', {
           duration: 3000,
         });
       },
       error => {
         console.error('Error saving result:', error);
-        // Show error snackBar
+   
         this.snackBar.open('Error al guardar el resultado', 'Cerrar', {
           duration: 3000,
         });
@@ -100,9 +120,136 @@ export class IngresoResultadosComponent implements OnInit {
     );
   }
 
+
+  actualizarCuartos(): void {
+    this.resultadoService.getResults().subscribe(results => {
+      const groups: { [key: string]: string[] } = {
+        'A': ['Argentina', 'Perú', 'Chile', 'Canadá'],
+        'B': ['México', 'Ecuador', 'Venezuela', 'Jamaica'],
+        'C': ['Estados Unidos', 'Uruguay', 'Panamá', 'Bolivia'],
+        'D': ['Brasil', 'Colombia', 'Paraguay', 'Costa Rica']
+      };
+  
+      const teams: { [key: string]: Team } = {};
+      const matchesPlayed: { [key: string]: number } = {};
+  
+      results.forEach((result: any) => {
+        if (!teams[result.equipo_local]) {
+          teams[result.equipo_local] = { nombre: result.equipo_local, puntos: 0, golesFavor: 0, golesContra: 0 };
+          matchesPlayed[result.equipo_local] = 0;
+        }
+        if (!teams[result.equipo_visitante]) {
+          teams[result.equipo_visitante] = { nombre: result.equipo_visitante, puntos: 0, golesFavor: 0, golesContra: 0 };
+          matchesPlayed[result.equipo_visitante] = 0;
+        }
+  
+        teams[result.equipo_local].golesFavor += result.goles_local;
+        teams[result.equipo_local].golesContra += result.goles_visitante;
+        teams[result.equipo_visitante].golesFavor += result.goles_visitante;
+        teams[result.equipo_visitante].golesContra += result.goles_local;
+  
+        matchesPlayed[result.equipo_local]++;
+        matchesPlayed[result.equipo_visitante]++;
+  
+        if (result.goles_local > result.goles_visitante) {
+          teams[result.equipo_local].puntos += 3;
+        } else if (result.goles_local < result.goles_visitante) {
+          teams[result.equipo_visitante].puntos += 3;
+        } else {
+          teams[result.equipo_local].puntos += 1;
+          teams[result.equipo_visitante].puntos += 1;
+        }
+      });
+  
+      const groupWinners: { [key: string]: Team } = {};
+      const groupRunnersUp: { [key: string]: Team } = {};
+
+      Object.keys(groups).forEach(groupKey => {
+        const groupTeams = groups[groupKey].map(teamName => teams[teamName]);
+  
+        const eligibleTeams = groupTeams.filter(team => matchesPlayed[team.nombre] === 3);
+  
+        const sortedTeams = eligibleTeams.sort((a, b) => {
+          if (b.puntos === a.puntos) {
+            return (b.golesFavor - b.golesContra) - (a.golesFavor - a.golesContra);
+          }
+          return b.puntos - a.puntos;
+        });
+  
+
+        if (sortedTeams.length > 0) {
+          groupWinners[groupKey] = sortedTeams[0];
+        } else {
+          console.error(`No hay suficientes equipos elegibles que hayan jugado 3 partidos en el grupo ${groupKey}.`);
+        }
+  
+ 
+        if (sortedTeams.length > 1) {
+          groupRunnersUp[groupKey] = sortedTeams[1];
+        } else {
+          console.error(`No hay suficientes equipos elegibles que hayan jugado 3 partidos en el grupo ${groupKey}.`);
+        }
+      });
+  
+      if (Object.keys(groupWinners).length === 4 && Object.keys(groupRunnersUp).length === 4) {
+   
+        this.cuartosFinales[0].equipo_local = groupWinners['A'].nombre;
+        this.cuartosFinales[0].equipo_visitante = groupRunnersUp['B'].nombre;
+        this.cuartosFinales[1].equipo_local = groupWinners['B'].nombre;
+        this.cuartosFinales[1].equipo_visitante = groupRunnersUp['A'].nombre;
+        this.cuartosFinales[2].equipo_local = groupWinners['C'].nombre;
+        this.cuartosFinales[2].equipo_visitante = groupRunnersUp['D'].nombre;
+        this.cuartosFinales[3].equipo_local = groupWinners['D'].nombre;
+        this.cuartosFinales[3].equipo_visitante = groupRunnersUp['C'].nombre;
+      } else {
+        console.error('No se encontraron suficientes ganadores o segundos lugares para los cuartos de final.');
+      }
+    }, error => {
+      console.error('Error al obtener los resultados de los partidos', error);
+    });
+  }
+
+
+  actualizarSemifinal(): void {
+    // Obtener los resultados de los cuartos de final
+    this.resultadoService.getResults().subscribe(results => {
+      const cuartosGanadores: { [key: number]: string } = {};
+  
+      // Asignar los equipos ganadores de cada partido de cuartos
+      this.cuartosFinales.forEach(cuarto => {
+        const result = results.find((r: { id_partido: number }) => r.id_partido === cuarto.id);
+        if (result) {
+          if (result.goles_local > result.goles_visitante) {
+            cuartosGanadores[cuarto.id] = cuarto.equipo_local;
+          } else {
+            cuartosGanadores[cuarto.id] = cuarto.equipo_visitante;
+          }
+        } else {
+          console.error(`No se encontró el resultado para el partido de cuartos con ID ${cuarto.id}`);
+        }
+      });
+  
+      // Asignar equipos y banderas a las semifinales
+      this.semifinales.forEach(semifinal => {
+        if (semifinal.id === 29) {
+          semifinal.equipo_local = cuartosGanadores[31]; // Ganador partido 1 cuartos
+          semifinal.equipo_visitante = cuartosGanadores[32]; // Ganador partido 2 cuartos
+        } else if (semifinal.id === 30) {
+          semifinal.equipo_local = cuartosGanadores[33]; // Ganador partido 3 cuartos
+          semifinal.equipo_visitante = cuartosGanadores[34]; // Ganador partido 4 cuartos
+        }
+      });
+  
+    }, error => {
+      console.error('Error al obtener los resultados de los cuartos de final', error);
+    });
+  }
+  
+  
+
+
   formatDate(dateString: string): string {
     const date = new Date(dateString);
-    // Obteniendo día y mes en formato 'DD/MM'
     const day = ('0' + date.getDate()).slice(-2);
     const month = ('0' + (date.getMonth() + 1)).slice(-2);
     return `${day}/${month}`;
@@ -117,7 +264,7 @@ export class IngresoResultadosComponent implements OnInit {
     return `assets/${this.teamFlags[team] || 'default.png'}`;
   }
 
-  // Función para verificar si localStorage está disponible
+
   isLocalStorageAvailable(): boolean {
     try {
       const test = '__storage_test__';
